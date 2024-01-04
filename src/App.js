97d848e092887e8c00d3b0e6c99cd8548ca92ae9
Grace Creatives <sumit.web6@gmail.com>
2024-01-04 19:57:55 +0530

    Designing Dashboard

--- src/App.js
+++ src/App.js
@@ -1,11 +1,18 @@
 import './App.css';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Register from './Components/Register';
+import Login from './Components/Login';
+import Dashboard from './Components/Dashboard';
 
 function App() {
   return (
-    
-    <Register/>
-    
+    <Router>
+      <Routes>
+        <Route path='/' element={<Register />} />
+        <Route path='/login' element={<Login />} />
+        <Route path='/home' element={<Dashboard />} />
+      </Routes>
+    </Router>
   );
 }
 
