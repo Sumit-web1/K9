1ae97082fcd7a825f386b212c5faafc3a8a14fda
Sumit-web1 <sumit.web6@gmail.com>
2024-01-31 19:05:06 +0530

    -

--- server/server.js
+++ server/server.js
@@ -6,21 +6,21 @@ const router = require("./router/auth-router");
 const connectDB = require("./utils/database");
 
 const corsOptions = {
-    origin: "*",
-    methods: ["GET", "POST", "PUT", "DELETE", "PATCH", "HEAD"],
-    credentials: true,
-};
+    origin: "http://localhost:3000",
+    method: "GET, POST, PUT, DELETE, PATCH, HEAD",
+    credetials: true,
+}
 
 app.use(cors(corsOptions));
 
 app.use(express.json());
 
-app.use("/api/auth", router);
+app.use("/api/auth", router)
 
 const PORT = 5000;
 
 connectDB().then(() => {
     app.listen(PORT, () => {
-        console.log(`Server is running at Port: ${PORT}`);
-    });
-});
+        console.log(`Server is running at Port : ${PORT}`)
+    })
+})
