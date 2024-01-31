7293031f7818759617fee1611f27908fbb82d832
Sumit-web1 <sumit.web6@gmail.com>
2024-01-31 18:58:46 +0530

    Updating Backend

--- server/server.js
+++ server/server.js
@@ -6,21 +6,21 @@ const router = require("./router/auth-router");
 const connectDB = require("./utils/database");
 
 const corsOptions = {
-    origin: "http://localhost:3000",
-    method: "GET, POST, PUT, DELETE, PATCH, HEAD",
-    credetials: true,
-}
+    origin: "*",
+    methods: ["GET", "POST", "PUT", "DELETE", "PATCH", "HEAD"],
+    credentials: true,
+};
 
 app.use(cors(corsOptions));
 
 app.use(express.json());
 
-app.use("/api/auth", router)
+app.use("/api/auth", router);
 
 const PORT = 5000;
 
 connectDB().then(() => {
     app.listen(PORT, () => {
-        console.log(`Server is running at Port : ${PORT}`)
-    })
-})
+        console.log(`Server is running at Port: ${PORT}`);
+    });
+});
